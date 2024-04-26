f7e238b6fc7109dcce45e9d3c998711c61e351e7
dev-rohit0 <rohit.codein@gmail.com>
2024-04-27 02:32:49 +0530

    some-fixes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const connectDB = async () => {
     try {
-        await mongoose.connect(`MONGO_URI=mongodb+srv://rowhit66:${process.env.MONGO_URI}@cluster0.preis8v.mongodb.net/questions?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://rowhit66:${process.env.MONGO_URI}@cluster0.preis8v.mongodb.net/questions?retryWrites=true&w=majority&appName=Cluster0`);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
